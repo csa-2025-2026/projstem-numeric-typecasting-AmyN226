b06f77f82648225414ec3d0e443bee5952ff04bb
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-09-09 21:46:22 +0000

    started

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,14 @@
+public class Problem2
+{
+    public static void main(String[] args)
+    {
+        int sum;
+        double num1 = 57.3934;
+        double num2 = 22.5211;
 
+        sum =
+
+
+
+    }
+}
