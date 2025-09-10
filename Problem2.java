293dfac5c8550c5a87d4acaa3f2998eadcfa5417
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-09-10 04:17:43 +0000

    finished

--- Problem2.java
+++ Problem2.java
@@ -2,11 +2,12 @@ public class Problem2
 {
     public static void main(String[] args)
     {
-        int sum;
+        
         double num1 = 57.3934;
         double num2 = 22.5211;
 
-        sum =
+        int sum = (int)(num1 + 0.5) + (int)(num2 + 0.5);
+        System.out.printf("\nThe whole number of %2.4f and %2.4f is %d\n", num1, num2, sum);
 
 
 
